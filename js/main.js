d2f2db0472d2d99c2912643f2dec5e943871124b
goesrafa <rafa.goes.2012@gmail.com>
2021-08-03 22:32:58 -0300

    up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+/*function soma(n1, n2){
+    return n1 + n2
+}
+
+function validaIdade(idade){
+    var validar;
+    if(idade >= 18){
+        validar = true;
+    }else{
+        validar = false
+    }
+    return validar;
+
+}
+
+var idade = prompt("Qual sua idade?") 
+console.log(validaIdade(idade))
+//alert(soma(5,10))
+*/
+
 /*var d = Date()
 alert(d);
 trabalho com data*/
