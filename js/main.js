d76acf480dc25673c3c355748ca5841f7481bb9a
goesrafa <rafa.goes.2012@gmail.com>
2021-08-03 22:04:23 -0300

    Dicionários

--- js/main.js
+++ js/main.js
@@ -1,15 +1,34 @@
+/*var carros = [{nome:"Renault", modelo: "Sandero"}, {nome:"Ford", modelo: "EcoSport"}]
+console.log(carros)
+alert(carros[1].modelo)
+*/
 
-var nome = "Rafaela Góes";
-var idade = 24;
-var idade2 = 10;
-var frase = "Corinthians tem mundial";
+/*var carro = {nome:"Renault", modelo: "Sandero"}
+console.log(carro.modelo)
+alert(carro.nome)
+*/ //Dicionário
+
+
+//var lista = ["Estudar", "Jogar", "Se formar"]
+//lista.push("Trabalhar")
+//lista.pop()
+//console.log(lista)
+//console.log(lista.toString())
+//console.log(lista.join(" - "))
+
+
+
+//var nome = "Rafaela Góes";
+//var idade = 24;
+//var idade2 = 10;
+//var frase = "Corinthians tem mundial";
 //alert(nome +  "tem" + idade + "anos");
 
 //alert(idade + idade2);
 
-console.log(nome);
-console.log(idade+idade2);
-console.log(frase.toUpperCase());//letras grandes
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.toUpperCase());//letras grandes
 
 //console.log(frase.toLowerCase());//letras pequenas
 
