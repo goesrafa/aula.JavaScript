a20dcf389cebf948e0ba54befb998389ae0a52c2
goesrafa <rafa.goes.2012@gmail.com>
2021-08-03 21:45:17 -0300

    primeiro código com javascript + html

---
+++ js/main.js
@@ -0,0 +1,17 @@
+
+var nome = "Rafaela Góes";
+var idade = 24;
+var idade2 = 10;
+var frase = "Corinthians tem mundial";
+//alert(nome +  "tem" + idade + "anos");
+
+//alert(idade + idade2);
+
+console.log(nome);
+console.log(idade+idade2);
+console.log(frase.toUpperCase());//letras grandes
+
+//console.log(frase.toLowerCase());//letras pequenas
+
+//alert(frase.replace("Palmeiras", "Corinthians"));
+
